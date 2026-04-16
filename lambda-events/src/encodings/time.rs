9543dc704ceb27914f86c00e53657c991ad19fab
DirkRusche-Otto <146933630+DirkRusche-Otto@users.noreply.github.com>
2026-04-16 08:30:37 +0200

    chore: remove non exhaustive from time structs (#1138)

--- lambda-events/src/encodings/time.rs
+++ lambda-events/src/encodings/time.rs
@@ -7,7 +7,6 @@ use serde::{
 use std::ops::{Deref, DerefMut};
 
 /// Timestamp with millisecond precision.
-#[non_exhaustive]
 #[derive(Clone, Default, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct MillisecondTimestamp(
     #[serde(deserialize_with = "deserialize_milliseconds")]
@@ -30,7 +29,6 @@ impl DerefMut for MillisecondTimestamp {
 }
 
 /// Timestamp with second precision.
-#[non_exhaustive]
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct SecondTimestamp(
     #[serde(deserialize_with = "deserialize_seconds")]
@@ -53,7 +51,6 @@ impl DerefMut for SecondTimestamp {
 }
 
 /// Duration with second precision.
-#[non_exhaustive]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct SecondDuration(
     #[serde(deserialize_with = "deserialize_duration_seconds")]
@@ -76,7 +73,6 @@ impl DerefMut for SecondDuration {
 }
 
 /// Duration with minute precision.
-#[non_exhaustive]
 #[derive(Clone, Default, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct MinuteDuration(
     #[serde(deserialize_with = "deserialize_duration_minutes")]
